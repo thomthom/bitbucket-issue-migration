5c885c42e0a8d74e91a882da8f7c7729969004ba
Thomas Thomassen <thomas@thomthom.net>
2020-05-19 21:10:48 +0200

    Allow for different source and target repo names.

--- migrate.rb
+++ migrate.rb
@@ -1,4 +1,5 @@
-repo_name = ARGV[0] or raise 'missing repository name'
-command = %[python migrate.py thomthom/#{repo_name} thomthom/#{repo_name} thomthom -bu thomthom --skip-attribution-for thomthom --mention-attachments]
+source_repo_name = ARGV[0] or raise 'missing repository name'
+target_repo_name = ARGV[1] || source_repo_name
+command = %[python migrate.py thomthom/#{source_repo_name} thomthom/#{target_repo_name} thomthom -bu thomthom --skip-attribution-for thomthom --mention-attachments]
 # puts command
 system(command)
